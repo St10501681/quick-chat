ef6933fa20bd18be2c87644d981737c79cc8a380
User <User@10.76.233.70>
2026-04-08 13:38:32 +0200

    add loops

--- src/project_part1/Project_part1.java
+++ src/project_part1/Project_part1.java
@@ -9,20 +9,35 @@ import java.util.Scanner;
  *
  * @author User
  */
+class Login{
+    boolean checkpin(String pin){
+        String name;
+        int password;
+        int cell_phone_number ;
+     for (password = 0; password <= 10; password ++) {
+            System.out.println(password);
+     }     
+}
 public class Project_part1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String name;
+        String name = null;
         int password;
+        int cell_number;
+       int length =name.length();
         
         
         
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your name");
-        String name = scan.nextLine();
+        String Name = scan.nextLine();
+        System.out.println("Enter password");
+        int Password = scan.nextInt();
+         System.out.println("Enter your cell phone number");
+         int Cell_Number = scan.nextInt();
         
                 
                 
